2b24dc5c96ff457be565f8da461f4887f6d6f43d
Ankush-Manhas840 <ankushmanhas643@gmail.com>
2024-09-09 07:54:29 +0530

    String basics

--- Practice_self/Print_all_Substring.java
+++ Practice_self/Print_all_Substring.java
@@ -1,2 +1,14 @@
-package Practice_self;public class Print_all_Substring {
+package Practice_self;
+import java.util.*;
+public class Print_all_Substring {
+    public static void main(String[] args) {
+        String ac ="abcd";
+        for(int i=0;i<=ac.length();i++){
+            for(int j=i+1;j<=ac.length();j++){
+                System.out.println(ac.substring(i,j));
+            }
+            System.out.println();
+        }
+
+    }
 }
